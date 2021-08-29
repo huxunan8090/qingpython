8553b3719250e53c1d33d5e394a60ee6e3fbf43e
huxunan <huxunan@gmail.com>
2021-08-30 04:06:44 +0800

    config

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   logo: 'https://z3.ax1x.com/2021/08/18/fITurD.jpg',
   outputPath: 'docs-dist',
   mode: 'site',
+  base: '/qingpython',
+  publicPath: '/qingpython/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  // 其他配置
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
