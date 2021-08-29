1c8ddfd87af5e20c4c2cb92d51e974f583acc378
huxunan <huxunan@gmail.com>
2021-08-30 04:15:56 +0800

    no message

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   outputPath: 'docs-dist',
   mode: 'site',
   base: '/qingpython',
-  // publicPath: '/',
+  publicPath: '/qingpython/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
   navs: [
